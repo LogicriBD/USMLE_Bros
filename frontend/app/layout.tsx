bfb7cda89d899210079f3934b12d3053d421574f
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2024-10-14 17:19:23 +0600

    frontend init

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Roboto_Slab } from "next/font/google";
+import metadataProperties from "@/metadata.json";
+
+const roboto = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = metadataProperties;
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
