888ebe0c16f742dd28ce93ba263f61959ddfae0f
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2024-10-22 15:04:01 +0600

    Categories modal

--- frontend/src/components/Content/Filter.tsx
+++ frontend/src/components/Content/Filter.tsx
@@ -1,21 +1,39 @@
+import { modalActions } from "@/src/context/store/slices/modal-slice";
+import { ModalName } from "@/utils/enums/ModalEnum";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 type FilterProps = {
     categories: string[];
 };
 
 const Filter = (props: FilterProps) => {
+
+    const dispatch = useDispatch();
+    const [searchText, setSearchText] = useState<string>("");
+
     return (
         <div className="flex flex-col w-full">
             <div className="w-full flex items-center justify-center">
-                <div className="relative w-1/2">
+                <div className="relative md:w-1/2 w-3/4">
                     <input
                         type="text"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
                         className="w-full p-2 px-4 border border-gray-400 bg-stone-200 hover:bg-stone-300 rounded-full focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent pr-10"
                         placeholder="Search"
                     />
-                    <FontAwesomeIcon icon={faSearch} className="absolute right-3 top-3 text-gray-400" />
+                    {searchText !== "" ? (
+                        <FontAwesomeIcon 
+                            icon={faSearch} 
+                            className="absolute right-3 top-3 text-gray-900 cursor-pointer" 
+                            onClick={() => console.log(searchText)}
+                        />
+                    ):(
+                        <FontAwesomeIcon icon={faSearch} className="absolute right-3 top-3 text-gray-400" />
+                    )}
                 </div>
             </div>
             <div className="flex items-center justify-center">
@@ -31,7 +49,9 @@ const Filter = (props: FilterProps) => {
                             {category}
                         </div>
                     ))}
-                    <div className="text-sky-100 bg-indigo-900 focus:bg-sky-300 hover:bg-indigo-800 focus:text-indigo-700 cursor-pointer font-bold text-sm rounded-full px-4 py-2 text-center">
+                    <div 
+                        onClick={() => dispatch(modalActions.updateModalType(ModalName.Categories))}
+                        className="text-sky-100 bg-indigo-900 focus:bg-sky-300 hover:bg-indigo-800 focus:text-indigo-700 cursor-pointer font-bold text-sm rounded-full px-4 py-2 text-center">
                         All
                     </div>
                 </div>
--- frontend/src/components/Modals/CategoriesModal.tsx
+++ frontend/src/components/Modals/CategoriesModal.tsx
@@ -1,14 +1,45 @@
 "use client";
 
+import { modalActions } from "@/src/context/store/slices/modal-slice";
+import { useState } from "react";
 import { Modal } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 
 const CategoriesModal = () => {
+    const [categories, setCategories] = useState<string[]>(["Anatomy", "Physiology", "Pathology", "Biochemistry"]);
+    const dispatch = useDispatch();
     return (
-        <Modal 
+        <Modal
             show={true}
-            onHide={() => {}}
-        ></Modal>
-        
+            onHide={() => dispatch(modalActions.removeModal())}
+            animation
+            centered
+            keyboard
+            className="rounded-xl h-96 w-96"
+        >
+            <Modal.Body className="p-0 rounded-xl">
+                <div className="flex flex-row w-full h-full min-h-96">
+                    <div className="w-1/4 min-h-full bg-indigo-900"></div>
+                    <div className="w-3/4 min-h-full bg-gray-100 flex flex-col">
+                        <div className="w-full flex items-center justify-center py-6">
+                            <h1 className="text-black font-semibold text-2xl">Select a category</h1>
+                        </div>
+                        <div className="w-full flex md:p-4 p-2 justify-center">
+                            <div className="grid md:grid-cols-3 md:gap-6 grid-cols-2 gap-2 justify-center mx-auto">
+                                {categories.map((category, index) => (
+                                    <div
+                                        key={index}
+                                        className={`text-sky-100 bg-indigo-900 hover:bg-indigo-800 focus:bg-sky-300 focus:text-indigo-700 cursor-pointer font-bold text-sm rounded-full md:px-4 px-2 py-2 text-center flex items-center justify-center`}
+                                    >
+                                        {category}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
     )
 }
 
