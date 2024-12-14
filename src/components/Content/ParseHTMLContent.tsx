7689bc6679a2de0f0b83068dfd47813ae4f69c47
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2024-12-14 20:45:49 +0600

    Handle math equations

--- src/components/Content/ParseHTMLContent.tsx
+++ src/components/Content/ParseHTMLContent.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import parse from 'html-react-parser';
+import { MathJax, MathJaxContext } from 'better-react-mathjax';
 import { useEffect } from 'react';
 import { useAppDispatch } from '@/src/context/store/hooks';
 import { loaderActions } from '@/src/context/store/slices/loader-slice';
@@ -9,16 +10,27 @@ interface IParseHTMLContent {
     id?: string;
     content: string;
 }
-
+const mergeFormulaTags = (html) => {
+    return html.replace(
+        /<p>\s*\\\[<\/p>\s*<p>(.*?)<\/p>\s*<p>\\\]<\/p>/gs,
+        '<p>\\[$1\\]</p>'
+    );
+};
 const ParseHTMLContent = ({ id, content }: IParseHTMLContent) => {
     const dispatch = useAppDispatch();
     useEffect(() => {
         dispatch(loaderActions.turnOff());
     }, [])
 
+    const processedContent = mergeFormulaTags(content);
+
     return (
         <div id={id} className='w-full content-display'>
-            {parse(content)}
+            <MathJaxContext>
+                <MathJax>
+                    {parse(processedContent)}
+                </MathJax>
+            </MathJaxContext>
         </div>
     )
 }
