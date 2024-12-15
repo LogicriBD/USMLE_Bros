f4c602634edd64631d64be876bae3c5b39648559
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2024-12-15 14:26:34 +0600

    fix ban

--- actions/chat/sendMessage.ts
+++ actions/chat/sendMessage.ts
@@ -1,4 +1,7 @@
 import { Chat } from "@/database/repository/Chat";
+import { User, UserData } from "@/database/repository/User";
+import { appStore } from "@/src/context/store/redux-store";
+import { userActions } from "@/src/context/store/slices/user-slice";
 import { Action } from "@/types/Action";
 import { SendMessage } from "@/types/Message";
 import { logger } from "@/utils/Logger";
@@ -8,6 +11,11 @@ export class sendMessage implements Action<void>{
 
     async execute(): Promise<void>{
         try{
+            const user:UserData = await User.findUserById(this.payload.message.userId);
+            if(user.banExpiry && user.banExpiry.toDate() > new Date()){
+                appStore.dispatch(userActions.setData(user));
+                return;
+            }
             await Chat.sendMessage(this.payload.message);
         }catch(error:any){
             logger.error(error);
